a275b145408577f5984643715b55e5285f0dbc87
Nick Martinovic <nmartinovicaa@gmail.com>
2023-12-13 11:48:35 +0100

    finished project

--- script.js
+++ script.js
@@ -13,15 +13,6 @@ function addBookToLibrary(book) {
 }
 
 
-const book1 = new Book('The Martian','Andy Weir',500,true);
-addBookToLibrary(book1)
-const book2 = new Book('Shadow Divers','Robert Kurson',600,true)
-addBookToLibrary(book2)
-const book3 = new Book('Recursion','Blake Crouch',400,true)
-addBookToLibrary(book3)
-const book4 = new Book('The Splendid and the Vile','Erik Larson',550,false)
-addBookToLibrary(book4)
-
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".add-book-button");
 const closeButton = document.querySelector(".close");
